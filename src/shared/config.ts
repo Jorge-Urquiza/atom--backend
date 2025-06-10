1688d625e5bf15913bcd72778916b11d43c6d3f7
Jorge Urquiza <jorgestudio2017@gmail.com>
2025-06-10 01:49:35 -0400

    feat: .env to save credentials

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 export const config = {
-    JWT_SECRET: 'atom-chat-secret'
-//   jwtSecret: process.env.JWT_SECRET || 'your-default-secret'
+  JWT_SECRET: process.env.JWT_SECRET || 'default-secret',
 };
